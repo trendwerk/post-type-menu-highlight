01e21ec780ce5bbb5865a18228171ecbb790a8d1
Harold Angenent <harold@trendwerk.nl>
2014-10-31 18:03:00 +0100

    Standalone-ish plugin

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-post-type-menu-highlight
+Post type menu highlight
 ========================
 
 Fixes classes in main menu. Made for WordPress.
+
+Assigns the right classes to post type archives in a WordPress menu.
+
+**This plugin requires TrendPress.**
---
+++ class-tp-post-type-menu-highlight.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: Post type menu highlight
+ * Description: Fixes classes in main menu. Assigns the right classes to post type archives in a WordPress menu.
+ */
+
+class TP_Post_Type_Menu_Highlight {
+	function __construct() {		
+		add_filter( 'wp_nav_menu_objects', array( $this, 'highlight' ) );
+	}
+
+	/**
+	 * Takes care of the highlighting in a navigation menu
+	 *
+	 * @param array $items
+	 */
+	function highlight( $items ) {
+		if( $items ) {
+			foreach( $items as $item ) {
+				if( in_array( 'current-menu-item', $item->classes ) )
+					return $items;
+			}
+			
+			$nav = new TP_Nav();
+			
+			if( is_single() || is_tax() || is_paged() || is_author() ) {
+				foreach( $items as &$item ) {
+					if( $nav->current_item == $item->ID )
+						$item->classes[] = 'current-menu-parent';
+				}
+			}
+		}
+		
+		return $items;
+	}
+} new TP_Post_Type_Menu_Highlight;
